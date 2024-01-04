3fffe15442741b97f093daa216199d581082a0ca
Khezac <khezac1@gmail.com>
2024-01-04 16:37:26 -0300

    feat.header

--- react-docs/src/components/layout/Container.js
+++ react-docs/src/components/layout/Container.js
@@ -1,6 +1,6 @@
 import styles from './Container.module.css'
 
-function Container({props}){
+function Container(props){
     return(
         <div className={`${styles.container} ${styles[props.customClass]}`}>
             {props.children}
--- react-docs/src/components/layout/Navbar.js
+++ react-docs/src/components/layout/Navbar.js
@@ -1,20 +1,28 @@
 import Container from './Container.js'
+import styles from './Navbar.module.css'
+import logo from '../../img/logo.png'
 
 function Navbar(){
     return(
-        <>
-            <Container>
-                <p>Logo</p>
-                <ul>
-                    <li>Home</li>
-                    <li>Produtos</li>
-                    <li>Carrinho</li>
-                    <li>Entrar</li>
-                    <li>Cadastrar</li>
-                </ul>
-            </Container>
-        </>
-    )
-}
+        <div className={styles.navbar_wrapper}>
+                <nav className={styles.navbar}>
+                    <img className={styles.logo} src={logo} alt='logo'/>
+                    <div className={styles.list_nav_options}>
+                        <ul className={styles.list}>
+                            <li className={styles.list_item}>Home</li>
+                            <li className={styles.list_item}>Produtos</li>
+                            <li className={styles.list_item}>Carrinho</li>
+                        </ul>
+                    </div>
+                    <div className={styles.list_signup_options}>
+                        <ul className={styles.list}>
+                            <li className={styles.list_item}>Entrar</li>
+                            <li className={styles.list_item}>Cadastrar</li>
+                        </ul>
+                    </div>
+                </nav>
+        </div>
+    
+)}
 
 export default Navbar
