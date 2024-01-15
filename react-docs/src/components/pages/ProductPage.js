cf32d8736a3b9e502f3b5e68ad310e91c4eb6fe1
Khezac <khezac1@gmail.com>
2024-01-15 15:18:29 -0300

    feat. starting-product-page

--- react-docs/src/components/pages/ProductPage.js
+++ react-docs/src/components/pages/ProductPage.js
@@ -1,4 +1,20 @@
+
+
 function ProductPage(){
-    return <h1>Produto X</h1>
+    return (
+        <div>
+            <div>
+                <img/>
+            </div>
+            <div>
+                <p>Nome do produto</p>
+                <p>Preço</p>
+                <div>
+                    <h3>Tamanhos:</h3>
+                    <Input/>
+                </div>
+            </div>
+        </div>
+    )
 }
 export default ProductPage
