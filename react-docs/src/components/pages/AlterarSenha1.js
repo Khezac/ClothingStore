a14a40d3137edb9035e79a935d11d6014fa1948c
Khezac <khezac1@gmail.com>
2024-02-06 11:26:13 -0300

    feat.send-confirmation-message-to-user-email

--- react-docs/src/components/pages/AlterarSenha1.js
+++ react-docs/src/components/pages/AlterarSenha1.js
@@ -2,12 +2,22 @@ import Input from "../form/Input"
 
 import styles from './AlterarSenha1.module.css'
 
+import { useState } from 'react'
+import emailjs from 'emailjs-com'
+
 function AlterarSenha1(){
+
+    const[email,setEmail] = useState()
+
+    function handleEmail(e){
+        setEmail({...email, [e.target.name]: e.target.value})   
+    }
+
     return (
         <div className={styles.alterar_senha1_wrapper}>
             <h1>Confirme seu e-mail</h1>
             <form className={styles.form_wrapper}>
-                <Input type='email' name='confirm_email' placeholder='Insira o e-mail da conta...'/>
+                <Input type='email' name='confirm_email' placeholder='Insira o e-mail da conta...' onChange={handleEmail}/>
                 <p>Enviaremos um e-mail de confirmação.</p>
             </form>   
         </div>
