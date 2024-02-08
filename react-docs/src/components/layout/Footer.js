66d606a3d9509a3fda2662e668944e807a4012fd
Khezac <khezac1@gmail.com>
2024-02-08 12:53:21 -0300

    fix.footer-links-when-not-loggedin

--- react-docs/src/components/layout/Footer.js
+++ react-docs/src/components/layout/Footer.js
@@ -1,18 +1,21 @@
 import Separator from './Separator';
 
-import footerlogo from '../../img/logo/logowriting.png' 
-import { FaGithub ,FaLinkedin } from "react-icons/fa";
+import footerlogo from '../../img/logo/logowriting.png'
+import { FaGithub, FaLinkedin } from "react-icons/fa";
 
 import styles from './Footer.module.css'
 
 import { Link } from 'react-router-dom'
 
-function Footer(){
+function Footer() {
+
+    const logged = JSON.parse(localStorage.getItem('checkUser'))
+
     return (
         <div className={styles.footer_wrapper}>
-            <Separator/>
+            <Separator />
             <div className={styles.footer_container}>
-                <img className={styles.footer_logo}src={footerlogo} alt='footer_logo'/>
+                <img className={styles.footer_logo} src={footerlogo} alt='footer_logo' />
                 <div className={styles.footer_info}>
                     <ul className={styles.footer_list}>
                         <li className={styles.item}><strong>Institucional</strong></li>
@@ -22,12 +25,26 @@ function Footer(){
                     </ul>
                     <ul className={styles.footer_list}>
                         <li className={styles.item}><strong>Ajuda</strong></li>
-                        <Link to='/atendimento'><li className={styles.item}>Trocas e Devoluções</li></Link>
-                        <Link to='/profile'><li className={styles.item}>Entregas</li></Link>
-                        <Link to='/profile'><li className={styles.item}>Minha Conta</li></Link>
-                        <Link to='/profile'><li className={styles.item}>Meus pedidos</li></Link>
-                        <Link to='/profile'><li className={styles.item}>Pagamentos</li></Link>
-                        <Link to='/profile'><li className={styles.item}>Cancelamentos</li></Link>
+                        {logged ? (
+                            <>
+                                <Link to='/atendimento'><li className={styles.item}>Trocas e Devoluções</li></Link>
+                                <Link to='/profile'><li className={styles.item}>Entregas</li></Link>
+                                <Link to='/profile'><li className={styles.item}>Minha Conta</li></Link>
+                                <Link to='/profile'><li className={styles.item}>Meus pedidos</li></Link>
+                                <Link to='/profile'><li className={styles.item}>Pagamentos</li></Link>
+                                <Link to='/profile'><li className={styles.item}>Cancelamentos</li></Link>
+                            </>
+                        ) : (
+                            <>
+                                <Link to='/atendimento'><li className={styles.item}>Trocas e Devoluções</li></Link>
+                                <Link to='/cadastro'><li className={styles.item}>Entregas</li></Link>
+                                <Link to='/cadastro'><li className={styles.item}>Minha Conta</li></Link>
+                                <Link to='/cadastro'><li className={styles.item}>Meus pedidos</li></Link>
+                                <Link to='/cadastro'><li className={styles.item}>Pagamentos</li></Link>
+                                <Link to='/cadastro'><li className={styles.item}>Cancelamentos</li></Link>
+                            </>
+                        )}
+
                     </ul>
                     <ul className={styles.footer_list}>
                         <li className={styles.item}><strong>Central de Atendimento</strong></li>
@@ -39,8 +56,8 @@ function Footer(){
                     </ul>
                 </div>
                 <div className={styles.footer_socialmedia}>
-                    <a href='https://www.github.com/khezac/' rel="noreferrer" target='_blank'><FaGithub className={styles.social_media_logo}/></a>
-                    <a href='https://www.linkedin.com/in/khezac/' rel="noreferrer" target='_blank'><FaLinkedin className={styles.social_media_logo}/></a>
+                    <a href='https://www.github.com/khezac/' rel="noreferrer" target='_blank'><FaGithub className={styles.social_media_logo} /></a>
+                    <a href='https://www.linkedin.com/in/khezac/' rel="noreferrer" target='_blank'><FaLinkedin className={styles.social_media_logo} /></a>
                 </div>
             </div>
         </div>
